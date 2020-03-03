1551b40b9beb03a3795eb1d7fed293e7d9c23193
Niklas Kobelev <niklas.kobelev@gmail.com>
2020-03-03 21:46:18 +0100

    add stats1, statsall

--- app.js
+++ app.js
@@ -1,66 +1,154 @@
-require('dotenv').config()
-const chalk = require('chalk')
-const notes = require('./notes.js')
-const Telegraf = require('telegraf')
+require("dotenv").config();
+const chalk = require("chalk");
+const notes = require("./notes.js");
+const Telegraf = require("telegraf");
 
-//
-const API_TOKEN = process.env.API_TOKEN || '';
-const URL = process.env.URL || 'https://telegram-bot-run.herokuapp.com/';
+// server variables
+const API_TOKEN = process.env.API_TOKEN || "";
+const URL = process.env.URL || "https://telegram-bot-run.herokuapp.com/";
 const PORT = process.env.PORT || 3000;
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// webhooks
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.command('modern', ({ reply }) => reply('Yo'))
-bot.hears('stats', (ctx) => {
-    const stats = notes.getLastXStats(10,ctx.chat.username)
-    console.log(chalk.blue(JSON.stringify(stats)))
-    ctx.reply(stats)
-})
+// app variables
+const STATSDAYS = 10;
+const DECIMALSAFTERDOT = 1;
+
+// bot commands
+bot.start(ctx => ctx.reply("Welcome"));
+bot.help(ctx => ctx.reply("Send me a sticker"));
+bot.on("sticker", ctx => ctx.reply("👍"));
+bot.hears("hi", ctx => ctx.reply("Hey there"));
+bot.command("modern", ({ reply }) => reply("Yo"));
+
+// stats
+bot.command("stats", ctx => {
+  ctx.reply(statsToStr(ctx.chat.username));
+});
+
+// stats all
+bot.command("statsall", ctx => {
+  ctx.reply(statsAll());
+});
+
+const statsAll = () => {
+  let users = notes.getAllUsers();
+  let outputStr = "";
+  users.forEach(username => {
+    let stats = notes.getAllStats(username);
+    console.log(chalk.yellow(JSON.stringify(stats)));
+    if (!stats.err) {
+      outputStr += username + ": ";
+      outputStr += stats.distance + "km, ";
+      outputStr += roundFloat(stats.duration / 60) + "h, ";
+      outputStr += roundFloat(stats.pace) + "km/min, ";
+      outputStr += stats.runs + " runs";
+    }
+  });
+  if (outputStr.length > 0) return outputStr;
+  else return "no data";
+};
+
+const statsToStr = username => {
+  const stats = notes.getLastXStats(STATSDAYS, username);
+  if (stats) {
+    let runsCnt = notes.getNrOfRuns(username);
+    replyStr = `Stats for \'${username}\':\n`;
+    replyStr += `Distance: ${stats.distance}km\n`;
+    replyStr += `Duration: ${stats.duration}min\n`;
+    let pace = notes.getPace(stats.distance, stats.duration);
+    if (stats.distance != 0) {
+      replyStr += `Pace: ${Number(pace.toFixed(DECIMALSAFTERDOT))}km/min\n`;
+    }
+    replyStr += `Runs: ${runsCnt}`;
+    return replyStr;
+  } else {
+    return "No runs saved";
+  }
+};
+
+const roundFloat = x => Number(x.toFixed(DECIMALSAFTERDOT));
+
+// delete all runs
+bot.command("deleteall", ctx => {
+  const deleted = notes.deleteAllStats(ctx.chat.username);
+  if (deleted) ctx.reply(`Removed all stats for user ${ctx.chat.username}`);
+  else ctx.reply(`No stats for user ${ctx.chat.username} found!`);
+});
+
+// add run
+bot.command("add", ctx => {
+  //ADD
+  if (ctx.message.text && ctx.message.text.includes("add")) {
+    let pace;
+    let paceCompare;
+    let statsNew;
+    let statsOld;
+    let added = false;
+    console.log(
+      "Message from user",
+      ctx.chat.username,
+      "recieved:",
+      ctx.message.text
+    );
+    var parts = ctx.message.text.split(" ");
+    console.log(parts);
+    if (parts.length > 1) {
+      var distance = 0;
+      var duration = 0;
+      parts.forEach(element => {
+        if (element.includes("km")) {
+          if (distance == 0) distance = parseFloat(element);
+        }
+        if (element.includes("m")) {
+          if (distance == 0) distance = parseFloat(element) / 1000;
+        }
+        if (element.includes("h")) {
+          if (duration == 0) duration = parseFloat(element) * 60;
+        }
+        if (element.includes("min")) {
+          if (duration == 0) duration = parseFloat(element);
+        }
+      });
+      if (distance != 0 && duration != 0) {
+        statsOld = notes.getLastXStats(STATSDAYS, ctx.chat.username);
+        notes.add(ctx.chat.username, distance, duration);
+        statsNew = notes.getLastXStats(STATSDAYS, ctx.chat.username);
+        pace = notes.getPace(distance, duration);
+        added = true;
+      }
+    }
+    if (added) {
+      if (statsOld) {
+        console.log("compare");
+        paceCompare = (1 - statsOld.pace / pace) * 100;
+      }
+      let replyStr = `Run added.\n`;
+      replyStr += `Distance: ${statsNew.distance}km\n`;
+      replyStr += `Duration: ${statsNew.duration}min\n`;
+      replyStr += `Pace(last run):\n${Number(pace.toFixed(1))}km/min`;
+      if (statsOld) {
+        if (paceCompare > 0)
+          replyStr += ` (+${Number(paceCompare.toFixed(DECIMALSAFTERDOT))}%)`;
+        else replyStr += ` (${Number(paceCompare.toFixed(DECIMALSAFTERDOT))}%)`;
+      }
+
+      return ctx.reply(replyStr);
+    } else {
+      return ctx.reply(`Invalid input format`);
+    }
+  }
+});
 
 bot.use((ctx, next) => {
-    if (!ctx.message) return next(ctx);
-    if (ctx.message.text == '/wipe') {
-        ctx.session = {}
-        return ctx.reply('session wiped').then(() => next(ctx))
-    }
+  if (!ctx.message) return next(ctx);
+  if (ctx.message.text == "/wipe") {
+    ctx.session = {};
+    return ctx.reply("session wiped").then(() => next(ctx));
+  }
+});
 
-    //ADD
-    if (ctx.message.text && ctx.message.text.includes("add")) {
-        console.log('Message from user', ctx.chat.username, 'recieved:', ctx.message.text)
-        var parts = ctx.message.text.split(" ")
-        console.log(parts)
-        if (parts.length > 1) {
-            var distance = 0
-            var duration = 0
-            parts.forEach(element => {
-                if (element.includes("km")) {
-                    if (distance == 0) distance = parseFloat(element)
-                }
-                if (element.includes("m")) {
-                    if (distance == 0) distance = parseFloat(element) / 1000
-                }
-                if (element.includes("h")) {
-                    if (duration == 0) duration = parseFloat(element) * 60
-                }
-                if (element.includes("min")) {
-                    if (duration == 0) duration = parseFloat(element)
-                }
-            });
-            notes.add(ctx.chat.username, distance, duration)
-        }
-        const notesList = notes.loadNotes()
-        return ctx.reply(notesList)
-    }
-
-//    return next(ctx)
-})
-
-
-
-bot.launch()
+bot.launch();
--- notes.js
+++ notes.js
@@ -1,149 +1,214 @@
-const chalk = require('chalk')
-const fs = require('fs')
+const chalk = require("chalk");
+const fs = require("fs");
 
 const add = (nickName, distance, duration) => {
-    const notes = loadNotes()
-    const duplicateNote = notes.find((note) => note.nickName === nickName)
+  const notes = loadNotes();
+  const duplicateNote = notes.find(note => note.nickName === nickName);
 
-    if (!duplicateNote) {
-        createNewNote(nickName, distance, duration)
-    } else {
-        addRun(nickName, distance, duration)
+  if (!duplicateNote) {
+    createNewNote(nickName, distance, duration);
+  } else {
+    addRun(nickName, distance, duration);
+  }
+};
+
+const getNrOfRuns = nickName => {
+  const notes = loadNotes();
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].nickName == nickName) {
+      return notes[i].runs.length;
     }
-}
+  }
+};
+
+const getAllUsers = () => {
+  const notes = loadNotes();
+  let users = [];
+  for (let i = 0; i < notes.length; i++) {
+    users.push(notes[i].nickName);
+  }
+  return users;
+};
 
 const createNewNote = (nickName, distance, duration) => {
-    const notes = loadNotes();
-    const runJson = createRun(distance, duration)
-    notes.push({
-        nickName: nickName,
-        stats: {
-            allDistance: distance,
-            allDuration: duration,
-            allPace: getPace(distance, duration)
-        },
-        runs: [runJson]
-    })
-    saveNotes(notes)
-    console.log(chalk.green.inverse('New note added!'))
-}
+  const notes = loadNotes();
+  const runJson = createRun(distance, duration);
+  notes.push({
+    nickName: nickName,
+    /*
+    stats: {
+      allDistance: distance,
+      allDuration: duration,
+      allPace: getPace(distance, duration)
+    },
+    */
+    runs: [runJson]
+  });
+  saveNotes(notes);
+  console.log(chalk.green.inverse("New note added!"));
+};
 
-const getJson1User = (userName) => {
-    const notes = loadNotes()
-    for (let i=0; i<notes.length; i++) {
-        if (notes[i].nickName == userName) {
-            return notes[i];
-        }
+const getJson1User = userName => {
+  const notes = loadNotes();
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].nickName == userName) {
+      return notes[i];
     }
-}
+  }
+};
+
+const getAllStats = userName => {
+  const stats = getJson1User(userName);
+  console.log(JSON.stringify(stats));
+  if (!stats) return { err: `no stats for user ${userName}` };
+  let sumDist = 0;
+  let sumDur = 0;
+  for (let i = 0; i < stats.runs.length; i++) {
+    console.log(stats.runs[i]);
+    sumDist += stats.runs[i].distance;
+    sumDur += stats.runs[i].duration;
+  }
+  return {
+    distance: sumDist,
+    duration: sumDur,
+    pace: getPace(sumDist, sumDur),
+    runs: stats.runs.length
+  };
+};
 
 const getLastXStats = (runsCnt, userName) => {
-    const lastNRuns = getLastNRuns(runsCnt, userName)
-    let sumDist = 0;
-    let sumDur = 0;
-    for (let i=0; i<lastNRuns.length; i++) {
-        console.log(lastNRuns[i])
-        sumDist += lastNRuns[i].distance
-        sumDur += lastNRuns[i].duration
-    }
-    return {distance:sumDist, duration:sumDur, pace:getPace(sumDist, sumDur)}
-}
+  const lastNRuns = getLastNRuns(runsCnt, userName);
+  if (!lastNRuns) return;
+  let sumDist = 0;
+  let sumDur = 0;
+  for (let i = 0; i < lastNRuns.length; i++) {
+    console.log(lastNRuns[i]);
+    sumDist += lastNRuns[i].distance;
+    sumDur += lastNRuns[i].duration;
+  }
+  return {
+    distance: sumDist,
+    duration: sumDur,
+    pace: getPace(sumDist, sumDur)
+  };
+};
 
 const getLastNRuns = (runsCnt, userName) => {
-    let lastXStats
-    const stats = getJson1User(userName).runs
-    const statsSorted = stats.sort((a, b) => a.date < b.date);
-    if (runsCnt == 0) {
-        return statsSorted
-    } else {
-        return statsSorted.slice(Math.max(statsSorted.length - runsCnt, 0))
-    }
-}
+  let lastXStats;
+  const stats = getJson1User(userName);
+  if (!stats) return;
+  const statsSorted = stats.runs.sort((a, b) => a.date < b.date);
+  if (runsCnt == 0) {
+    return statsSorted;
+  } else {
+    return statsSorted.slice(Math.max(statsSorted.length - runsCnt, 0));
+  }
+};
 
-const getPace = (distance, duration) => duration / distance
+const deleteAllStats = userName => {
+  const notes = loadNotes();
+  if (!notes) return false;
+  else {
+    for (let i = 0; i < notes.length; i++) {
+      if (notes[i].nickName == userName) {
+        notes[i].stats = [];
+        notes[i].runs = [];
+        saveNotes(notes);
+        return true;
+      }
+    }
+  }
+  console.log("not deleted");
+  return false;
+};
+
+const getPace = (distance, duration) => duration / distance;
 
 const createRun = (distance, duration) => {
-    const pace = getPace(distance, duration)
-    var date = new Date();
-    return {
-        date: date,
-        distance: distance,
-        duration: duration,
-        pace: pace
-    }
-}
+  const pace = getPace(distance, duration);
+  var date = new Date();
+  return {
+    date: date,
+    distance: distance,
+    duration: duration,
+    pace: pace
+  };
+};
 
 const addRun = (nickNameGiven, distance, duration) => {
-    const notes = loadNotes()
-    var newRun = createRun(distance, duration)
-    for (let i=0; i<notes.length; i++) {
-        if (notes[i].nickName == nickNameGiven) {
-            notes[i].stats.allDistance = notes[i].stats.allDistance + distance
-            notes[i].stats.allDuration = notes[i].stats.allDuration + duration
-            notes[i]['runs'].push(newRun);
-            break;
-        }
+  const notes = loadNotes();
+  var newRun = createRun(distance, duration);
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].nickName == nickNameGiven) {
+      notes[i].stats.allDistance = notes[i].stats.allDistance + distance;
+      notes[i].stats.allDuration = notes[i].stats.allDuration + duration;
+      notes[i]["runs"].push(newRun);
+      break;
     }
-    console.log(chalk.green.inverse('Stats added!'))
-    saveNotes(notes)
-}
+  }
+  console.log(chalk.green.inverse("Stats added!"));
+  saveNotes(notes);
+};
 
-const removeNote = (nickName) => {
-    const notes = loadNotes()
-    const notesToKeep = notes.filter((note) => note.nickName !== nickName)
+const removeNote = nickName => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(note => note.nickName !== nickName);
 
-    if (notes.length > notesToKeep.length) {
-        console.log(chalk.green.inverse('Note removed!'))
-        saveNotes(notesToKeep)
-    } else {
-        console.log(chalk.red.inverse('No note found!'))
-    }    
-}
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed!"));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No note found!"));
+  }
+};
 
 const listNotes = () => {
-    const notes = loadNotes()
+  const notes = loadNotes();
 
-    console.log(chalk.inverse('Your notes'))
+  console.log(chalk.inverse("Your notes"));
 
-    notes.forEach((note) => {
-        console.log(note.nickName)
-    })
-}
+  notes.forEach(note => {
+    console.log(note.nickName);
+  });
+};
 
-  
+const readNote = nickName => {
+  const notes = loadNotes();
+  const note = notes.find(note => note.nickName === nickName);
 
-const readNote = (nickName) => {
-    const notes = loadNotes()
-    const note = notes.find((note) => note.nickName === nickName)
+  if (note) {
+    console.log(chalk.inverse(note.nickName));
+    console.log(note.distance);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
 
-    if (note) {
-        console.log(chalk.inverse(note.nickName))
-        console.log(note.distance)
-    } else {
-        console.log(chalk.red.inverse('Note not found!'))
-    }
-}
-
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-}
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
 const loadNotes = () => {
-    try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
-    }
-}
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
 
 module.exports = {
-    add: add,
-    removeNote: removeNote,
-    listNotes: listNotes,
-    readNote: readNote,
-    loadNotes: loadNotes,
-    getLastXStats: getLastXStats
-}
+  add: add,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+  loadNotes: loadNotes,
+  getLastXStats: getLastXStats,
+  getPace: getPace,
+  deleteAllStats: deleteAllStats,
+  getNrOfRuns,
+  getAllUsers,
+  getAllStats
+};
