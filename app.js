a205aa691802972093ac11c9c4e50694ea621998
Niklas Kobelev <niklas.kobelev@gmail.com>
2020-03-21 18:26:49 +0100

    add history command

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const Telegraf = require("telegraf");
 const extra = require("telegraf/extra");
 const markup = extra.markdown();
 var table = require("markdown-table");
-var width = require("string-width");
 
 // server variables
 const API_TOKEN = process.env.API_TOKEN || "";
@@ -22,50 +21,101 @@ const STATSDAYS = 10;
 const DECIMALSAFTERDOT = 1;
 const MAX_CHARS_NAME = 5;
 
-// bot commands
+// bot commands ========================================================================
 bot.start(ctx => ctx.reply("Welcome"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears("hi", ctx => ctx.reply("Hey there"));
 bot.command("modern", ({ reply }) => reply("Yo"));
 
-// send markdown text
+// test ===============================================================================
 bot.hears("test", ctx =>
   bot.telegram.sendMessage((chatid = ctx.from.id), "*me work!work!*", markup)
 );
 
-// stats
+// stats: me =========================================================================
 bot.command("me", ctx => {
-  ctx.reply(statsToStr(ctx.chat.username));
+  let username = ctx.chat.username;
+  let tableData = stats1UserRows(username);
+  let tableOptions = { delimiterStart: false, delimiterEnd: false };
+  let tableAsStr = table(tableData, tableOptions);
+  let titleText = `stats *${prepairStrForMarkdown(username)}*:\n`;
+  let tableNoHeader = tableAsStr.substring(
+    tableAsStr.indexOf("km"),
+    tableAsStr.length
+  ); //remove everthing from the table until the first row
+  bot.telegram.sendMessage(
+    (chatid = ctx.from.id),
+    (text = titleText.concat(addMarkdownEvenWidth(tableNoHeader))),
+    markup
+  );
 });
 
-const prepairStrForMarkdown = input => {
-  let result = input;
-  result = result.replace("*", "*");
-  result = result.replace("_", "_");
-  result = result.replace("`", "`");
-  return result;
-};
+// history =========================================================================
+bot.command("history", ctx => {
+  let username = ctx.chat.username;
 
-const addMarkdownEvenWidth = input => "```\n".concat(input).concat("\n```");
+  // get amount of runs
+  let runsAmount = 0;
+  if (ctx.message.text.split(" ").length == 2) {
+    let tmp = ctx.message.text.split(" ")[1];
+    console.log(tmp);
+    console.log(typeof tmp);
+    if (!isNaN(tmp)) runsAmount = parseInt(tmp);
+  }
 
-// stats all
+  let historyData = history1UserRow(ctx.chat.username, runsAmount);
+  let tableOptions = { delimiterStart: false, delimiterEnd: false };
+  let tableAsStr = table(historyData, tableOptions);
+
+  let titleText = `history *${prepairStrForMarkdown(username)}*`;
+  if (runsAmount == 0) {
+    titleText += `, all runs:\n`;
+  } else {
+    titleText += `, last ${runsAmount} runs:\n`;
+  }
+
+  bot.telegram.sendMessage(
+    (chatid = ctx.from.id),
+    (text = titleText.concat(addMarkdownEvenWidth(tableAsStr))),
+    markup
+  );
+});
+
+// stats: all =========================================================================
 bot.command("all", ctx => {
   let stats = statsAll();
   console.log(stats);
 
   let tableData = statsAllToRows();
   let tableOptions = { delimiterStart: false, delimiterEnd: false };
-  let tableStr = table(tableData, {
-    delimiterStart: false,
-    delimiterEnd: false
-  });
+  let tableAsStr = table(tableData, tableOptions);
+
   bot.telegram.sendMessage(
     (chatid = ctx.from.id),
-    addMarkdownEvenWidth(tableStr),
+    (text = addMarkdownEvenWidth(tableAsStr)),
     markup
   );
 });
+
+// functions =========================================================================
+const history1UserRow = (username, runsAmount) => {
+  let runs = notes.getLastNRuns(runsAmount, username);
+  let rowHeader = ["date", "km", "h", "m/km"];
+  let rows = [rowHeader];
+  console.log(JSON.stringify(runs));
+  runs.forEach(runStats => {
+    let date = new Date(runStats.date);
+    let row = [
+      date.getDate() + "." + (date.getMonth() + 1),
+      runStats.distance,
+      runStats.duration,
+      runStats.pace
+    ];
+    rows.push(row);
+  });
+  return rows;
+};
 
 const statsAllToRows = () => {
   let rowHeader = ["name", "km", "h", "m/km"];
@@ -78,12 +128,30 @@ const statsAllToRows = () => {
       let row = [prepairStrForMarkdown(username.substring(0, MAX_CHARS_NAME))];
       row.push(roundFloat(stats.distance));
       row.push(roundFloat(stats.duration / 60));
-      row.push(roundFloat(stats.pace));
+      if (stats.distance > 0) row.push(roundFloat(stats.pace));
       rows.push(row);
     }
   });
   if (users.length > 0) return rows;
   else return "no data";
+};
+
+const stats1UserRows = username => {
+  const stats = notes.getLastXStats(STATSDAYS, username);
+  if (stats) {
+    let rows = [["stat", "value"]];
+    rows.push(["km", `${stats.distance}`]);
+    rows.push([`h`, `${roundFloat(stats.duration / 60)}`]);
+    if (stats.distance > 0) {
+      let pace = notes.getPace(stats.distance, stats.duration);
+      rows.push([`km/min`, `${Number(pace.toFixed(DECIMALSAFTERDOT))}`]);
+    }
+    rows.push([`runs`, `${notes.getNrOfRuns(username)}`]);
+    console.log(JSON.stringify(rows));
+    return rows;
+  } else {
+    return { error: "No runs saved" };
+  }
 };
 
 const statsAll = () => {
@@ -101,7 +169,7 @@ const statsAll = () => {
     }
   });
   if (outputStr.length > 0) return outputStr;
-  else return "no data";
+  else return { error: "no data" };
 };
 
 const statsToStr = username => {
@@ -155,9 +223,6 @@ bot.command("add", ctx => {
         if (element.includes("km")) {
           if (distance == 0) distance = parseFloat(element);
         }
-        if (element.includes("m")) {
-          if (distance == 0) distance = parseFloat(element) / 1000;
-        }
         if (element.includes("h")) {
           if (duration == 0) duration = parseFloat(element) * 60;
         }
@@ -178,7 +243,7 @@ bot.command("add", ctx => {
         console.log("compare");
         paceCompare = (1 - statsOld.pace / pace) * 100;
       }
-      let replyStr = `Run added.\n`;
+      let replyStr = `run added.\n`;
       replyStr += `Distance: ${statsNew.distance}km\n`;
       replyStr += `Duration: ${statsNew.duration}min\n`;
       replyStr += `Pace(last run):\n${Number(pace.toFixed(1))}km/min`;
@@ -202,5 +267,15 @@ bot.use((ctx, next) => {
     return ctx.reply("session wiped").then(() => next(ctx));
   }
 });
+
+const prepairStrForMarkdown = input => {
+  let result = input;
+  result = result.replace("*", "*");
+  result = result.replace("_", "_");
+  result = result.replace("`", "`");
+  return result;
+};
+
+const addMarkdownEvenWidth = input => "```\n".concat(input).concat("\n```");
 
 bot.launch();
